3a8207502804232afb208d459baa93333e6ba1c0
acbuie <aidancbuie@gmail.com>
2024-01-23 11:57:16 -0700

    feat: Build urls with real API keys
    Load API keys from a .env file in the root package directory.

--- src/dicc/url.py
+++ src/dicc/url.py
@@ -3,9 +3,23 @@ from collections.abc import Callable
 from typing import Literal
 
 import httpx
+from dotenv import dotenv_values
 
 QueryProtocol = Callable[[str, str], httpx.URL]
 QueryMethod = Literal["dictionary", "thesaurus"]
+
+
+def load_api_keys() -> tuple[str, str]:
+    """Load the API key from .env file."""
+    keys = dotenv_values(".env")
+
+    dict_key = keys.get("DICTIONARY_KEY")
+    thes_key = keys.get("THESAURUS_KEY")
+
+    if not dict_key or not thes_key:
+        raise ValueError("API keys not found.")
+
+    return (dict_key, thes_key)
 
 
 def _dictionary_url(word: str, api_key: str) -> httpx.URL:
@@ -32,10 +46,12 @@ def _thesaurus_url(word: str, api_key: str) -> httpx.URL:
 
 def _url_query_factory(method: QueryMethod) -> tuple[QueryProtocol, str]:
     """Provide the proper query function and api key for a query method."""
+    keys = load_api_keys()
+
     if method == "dictionary":
-        return _dictionary_url, "dictionary_api"
+        return _dictionary_url, keys[0]
     elif method == "thesaurus":
-        return _thesaurus_url, "thesaurus_api"
+        return _thesaurus_url, keys[1]
     else:
         raise ValueError("Invalid query method.")
 
@@ -45,9 +61,3 @@ def build_url(word: str, method: QueryMethod) -> httpx.URL:
     url_query_callable, api_key = _url_query_factory(method)
     url = url_query_callable(word, api_key)
     return url
-
-
-if __name__ == "__main__":
-    url = build_url("blue", "thesaurus")
-
-    print(url)
